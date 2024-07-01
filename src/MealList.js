f39d406cfee791e58c7ca5e1c88b9f68e12b23a0
tippytap <erikmiller6@gmail.com>
2024-07-01 15:13:34 -0400

    Changed filter UI
    Updated from buttons to Menu with menuitems

--- src/MealList.js
+++ src/MealList.js
@@ -1,9 +1,11 @@
-import React from 'react';
-import { Box, Button, Icon, Stack, Typography, TextField, Card, CardHeader, CardContent, Container, Grid } from '@mui/material';
+import React, { useState } from 'react';
+import { Box, Button, Icon, Stack, Typography, TextField, Card, CardHeader, CardContent, Container, Grid, Menu, MenuItem, Divider } from '@mui/material';
 import Meal from './Meal';
 import { useMealContext } from './MealContext';
 
 export default function MealList(props) {
+
+    const { filter } = useMealContext();
 
     const formatName = (name, i) => {
         let formattedName = name + "-" + i
@@ -14,6 +16,9 @@ export default function MealList(props) {
         <Container maxWidth="xl">
             <MealFilterButtons />
             <Grid container spacing={2} alignItems="stretch" mt={5}>
+                <Container>
+                    <Typography variant="h4">{filter !== "all" && filter.toUpperCase()}</Typography>
+                </Container>
                 {props.meals.map((meal, i) => {
                     return (
                         <Grid key={i} item xs={4}>
@@ -35,18 +40,48 @@ export default function MealList(props) {
 export const MealFilterButtons = () => {
 
     const {filter, updateFilter, categories} = useMealContext();
+    const [anchorEl, setAnchorEl] = useState(null);
+    const open = Boolean(anchorEl);
 
     const handleClick = (e) => {
-        updateFilter(e.target.name);
+        setAnchorEl(e.currentTarget);
+    }
+
+    const handleClose = () => {
+        setAnchorEl(null);
+    }
+
+    const handleCategoryClick = (e) => {
+        updateFilter(e.target.dataset.category);
+        handleClose();
     }
 
     return (
         <Stack direction="row" spacing={2} justifyContent={"center"} mt={3}>
+            <Button 
+                id="category-button" 
+                aria-controls={open ? 'category-menu' : undefined}
+                aria-haspopup="true"
+                aria-expanded={open ? 'true' : undefined}
+                onClick={handleClick}
+            >
+               Categories 
+            </Button>
+            <Menu
+                id="basic-menu"
+                anchorEl={anchorEl}
+                open={open}
+                onClose={handleClose}
+                MenuListProps={{
+                    'aria-labelledby': 'category-button',
+                }}
+            >
             {categories.map((category) => {
                 return (
-                    <Button variant={filter === category ? "contained" : "text"} onClick={handleClick} name={category} key={category}>{category}</Button>
+                    <MenuItem onClick={handleCategoryClick} data-category={category} name={category} key={category}>{category.toUpperCase()}</MenuItem>
                 )
             })}
+            </Menu>
         </Stack>
     )
 }
